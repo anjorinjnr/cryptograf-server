f11c1bfbb5037a3dfb6394002427ff95dd766bd2
richwednesday <iwokjoseph@gmail.com>
2018-01-06 14:50:12 +0100

    getting the data, saving to ndb next

--- handlers/task_handler.py
+++ handlers/task_handler.py
@@ -1,13 +1,20 @@
-
+from google.appengine.api import urlfetch
 from handlers.base_handler import BaseHandler
 
 class FetchCoinTickerHandler(BaseHandler):
-
-  def post(self):
-    """This endpoint will be called by a cron job."""
-    #TODO(joshua) implement code to pull data and save to ndb.
-
-    self.error_response('NOT YET IMPLEMENTED!!!')
+	def post(self):
+		"""This endpoint will be called by a cron job."""
+		#TODO(joseph) implement code to pull data and save to ndb.
+		url = 'https://api.coinmarketcap.com/v1/ticker/'
+		try:
+			result = urlfetch.fetch(url)
+			if result.status_code == 200:
+				self.response.write(result.content)
+			else:
+				self.response.status_code = result.status_code
+		except urlfetch.Error:
+			logging.exception('Caught exception fetching url')
+			self.error_response('ERROR FETCHING DATA!!!')
 
 
 ROUTES = [
